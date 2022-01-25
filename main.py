f11464a9e9e50e13d6ac1c756fe1ce7f26f709cb
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 19:44:21 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from math import *
+
+# import external code. "math" function
+
+print(floor(3.7))
+
+# "floor" eliminate small point, "ceil" for an opposite method
+# "sqrt" square root the number , you can search or math function in python
